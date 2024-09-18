38acd3498aba4fe8100f70883bce592267902879
userkamaljha <userkamaljha@gmail.com>
2024-09-18 15:05:49 +0530

    Project setup commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+function HomePage() {
+  return (
+    <div>
+      This is Home Page
+      <br />
+      <Button variant={'destructive'}>Click Here</Button>
+    </div>
+  )
+}
+
+export default HomePage
