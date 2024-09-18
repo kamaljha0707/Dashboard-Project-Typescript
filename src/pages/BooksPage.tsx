4e7130788f032f690662279e64fbabc69ff2276d
userkamaljha <userkamaljha@gmail.com>
2024-09-18 16:43:27 +0530

    add Dashboard Layout

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button"
 
 
-function HomePage() {
+function BooksPage() {
   return (
-   <>
-        <div className="flex items-center">
-          <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
+    <>
+    <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl">Books</h1>
         </div>
         <div
           className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
@@ -20,8 +20,8 @@ function HomePage() {
             <Button className="mt-4">Add Book</Button>
           </div>
         </div>
-   </>
+    </>
   )
 }
 
-export default HomePage
+export default BooksPage
--- src/router.tsx
+++ src/router.tsx
@@ -2,16 +2,32 @@ import {createBrowserRouter} from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
 import HomePage from "@/pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardLayout from "./components/ui/layouts/DashboardLayout";
+import BooksPage from "./pages/BooksPage";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: < HomePage />
+      element: <DashboardLayout/>,
+      children:[
+        {
+          path: 'home',
+         element: < HomePage />
+        },
+        {
+          path: 'books',
+         element: < BooksPage />
+        },
+      ],
     },
     {
       path: "/login",
       element: < LoginPage />
+    },
+    {
+      path: "/register",
+      element: < RegisterPage />
     },
     {
       path: "/register",
